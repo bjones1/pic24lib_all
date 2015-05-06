57c343ad370e0310e3e4d1b5f6fa5c1e8fe4899a
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-05-06 14:27:12 -0500

    Del: Old Windows bootloader (now using multi-platform Qt-based bootloader).

--- bootloader/winbootldr/bootloader.cpp
+++
@@ -1,9 +0,0 @@
-#include "StdAfx.h"
-#include "bootloader.h"
-
-bootloader::bootloader(void)
-{
-
-
-
-}
--- bootloader/winbootldr/unitTests/unitTestRunner.cpp
+++
@@ -1,6 +0,0 @@
-/** \file
- *  \brief Runs the unit test on MSVC.
- */
-
-#include "stdafx.h"
-#include "unitTestRunner.h"
