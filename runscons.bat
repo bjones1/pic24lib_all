aafc88bd80c2af92f3e3acc1138dffb7467b9e83
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-10-23 16:35:31 -0500

    Docs: Use REM as a batch file comment, per updated CodeChat.

--- runscons.bat
+++ runscons.bat
@@ -1,44 +1,44 @@
-: .. "Copyright (c) 2008 Robert B. Reese, Bryan A. Jones, J. W. Bruce ("AUTHORS")"
-:    All rights reserved.
-:    (R. Reese, reese_AT_ece.msstate.edu, Mississippi State University)
-:    (B. A. Jones, bjones_AT_ece.msstate.edu, Mississippi State University)
-:    (J. W. Bruce, jwbruce_AT_ece.msstate.edu, Mississippi State University)
-:
-:     Permission to use, copy, modify, and distribute this software and its
-:     documentation for any purpose, without fee, and without written agreement is
-:     hereby granted, provided that the above copyright notice, the following
-:     two paragraphs and the authors appear in all copies of this software.
-:
-:     IN NO EVENT SHALL THE "AUTHORS" BE LIABLE TO ANY PARTY FOR
-:     DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
-:     OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE "AUTHORS"
-:     HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-:
-:     THE "AUTHORS" SPECIFICALLY DISCLAIMS ANY WARRANTIES,
-:     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-:     AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
-:     ON AN "AS IS" BASIS, AND THE "AUTHORS" HAS NO OBLIGATION TO
-:     PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS."
-:
-:     Please maintain this header in its entirety when copying/modifying
-:     these files.
-:
-: .. highlight:: bat
-:
-: ***********************************************************
-: runscons.bat - Run SCons, collecting and filtering warnings
-: ***********************************************************
-: First, run the build, collecting all errors and warnings into ``err.txt``.
+REM .. "Copyright (c) 2008 Robert B. Reese, Bryan A. Jones, J. W. Bruce ("AUTHORS")"
+REM    All rights reserved.
+REM    (R. Reese, reese_AT_ece.msstate.edu, Mississippi State University)
+REM    (B. A. Jones, bjones_AT_ece.msstate.edu, Mississippi State University)
+REM    (J. W. Bruce, jwbruce_AT_ece.msstate.edu, Mississippi State University)
+REM
+REM     Permission to use, copy, modify, and distribute this software and its
+REM     documentation for any purpose, without fee, and without written agreement is
+REM     hereby granted, provided that the above copyright notice, the following
+REM     two paragraphs and the authors appear in all copies of this software.
+REM
+REM     IN NO EVENT SHALL THE "AUTHORS" BE LIABLE TO ANY PARTY FOR
+REM     DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+REM     OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE "AUTHORS"
+REM     HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+REM
+REM     THE "AUTHORS" SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+REM     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+REM     AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+REM     ON AN "AS IS" BASIS, AND THE "AUTHORS" HAS NO OBLIGATION TO
+REM     PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS."
+REM
+REM     Please maintain this header in its entirety when copying/modifying
+REM     these files.
+REM
+REM .. highlight:: bat
+REM
+REM ***********************************************************
+REM runscons.bat - Run SCons, collecting and filtering warnings
+REM ***********************************************************
+REM First, run the build, collecting all errors and warnings into ``err.txt``.
 call scons -j 1 %* > err.txt 2>&1
-:
-: Remove #warnings generated during compiles
-: ==========================================
-: Note: the ``> errn.txt`` was placed immediately after findstr below, since the
-: use of ``\"`` confuses cmd.exe (but not findstr). Placing it at the end
-: prevents cmd.exe from capturing output to the ``errn.txt`` file.
-:
-: Likewise, I can't use the ^ line continuation character following lines where
-: the ``"`` symbols don't match.
+REM
+REM Remove #warnings generated during compiles
+REM ==========================================
+REM Note: the ``> errn.txt`` was placed immediately after findstr below, since the
+REM use of ``\"`` confuses cmd.exe (but not findstr). Placing it at the end
+REM prevents cmd.exe from capturing output to the ``errn.txt`` file.
+REM
+REM Likewise, I can't use the ^ line continuation character following lines where
+REM the ``"`` symbols don't match.
 findstr > err1.txt /V ^
 /C:"warning: Options have been disabled due to expired license" ^
 /C:"#warning \"Baud rates of 19200 or lower recommended for this clock choice.\""  ^
@@ -72,20 +72,16 @@ findstr > err2.txt /V ^
 /C:"warning: #warning Building configUART1() for the Rev.C1 Embedded Systems target." ^
 /C:"warning: #warning Building configUART1() for the StarterBoard_28P target." ^
 /C:"#warning \"Ensure that the PLLDIV value is set to divide by 2 in the configuration bits for " err1.txt
-:
-: Work around compiler warnings I can't figure out how to suppress
-: ================================================================
+REM
+REM Work around compiler warnings I can't figure out how to suppress
+REM ================================================================
 findstr > err3.txt /V ^
 /C:"\bootloader\pic24_dspic33_bootloader.X\mem.c:106:1: warning: control reaches end of non-void function" ^
 /C:"warning: Taking the address of '" ^
 err2.txt
-
-
-
-
-:
-: Output only errors and warnings.
-: ================================
+REM
+REM Output only errors and warnings
+REM ===============================
 findstr /C:"warning" /C:"error" err3.txt > build_errors.txt
 echo Errors and warnings:
 type build_errors.txt
